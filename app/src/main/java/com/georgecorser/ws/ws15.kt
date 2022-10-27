39e69d79af023a884c9d1b7913d6732da32f2e2d
George Corser <gpcorser@svsu.edu>
2022-10-27 18:58:11 -0400

    screens 10-19, empty

---
+++ app/src/main/java/com/georgecorser/ws/ws15.kt
@@ -0,0 +1,11 @@
+package com.georgecorser.ws
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ws15 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ws15)
+    }
+}
