d1e9a93cfb1dbda03d37a47e4b72176c4fc94254
George Corser <gpcorser@svsu.edu>
2022-10-27 18:49:34 -0400

    first 10 screens, empty

---
+++ app/src/main/java/com/georgecorser/ws/ws01.kt
@@ -0,0 +1,11 @@
+package com.georgecorser.ws
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ws01 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ws01)
+    }
+}
